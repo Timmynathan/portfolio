e74dbc943cffcc4a870acaae3ac6a1492db05ac6
Timmynathan <oluwatimilehin.ilesanmi@pau.edu.ng>
2025-12-20 08:45:59 +0100

    Modify NavBar components for easy homepage navigation

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,9 +1,13 @@
+import Link from "next/link";
 import styles from "./Navbar.module.css";
 
 export default function Navbar() {
   return (
     <header className={styles.navbar}>
-      <h1>Ilesanmi Oluwatimilehin.</h1>
+      <Link href="/" className={styles.brand}>
+        <strong><h1>Ilesanmi Oluwatimilehin.</h1> </strong>
+      </Link>
+
       <nav className={styles.navLinks}>
         <a href="#about">About</a>
         <a href="#projects">Projects</a>
